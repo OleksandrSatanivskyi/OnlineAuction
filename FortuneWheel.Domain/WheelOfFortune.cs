3294a09f840e53dbe76a02b1c2455d2cec59a4bd
Sasha <oleksandrsy@gmail.com>
2024-01-14 17:15:02 +0200

    Created WheelOfFortune class

---
+++ FortuneWheel.Domain/WheelOfFortune.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FortuneWheel.Domain
+{
+    public class WheelOfFortune
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public List<WheelSegment> Segments { get; set; }
+    }
+
+}
