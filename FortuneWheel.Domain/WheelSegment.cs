42930c7f57a064fb1a370dedeb8e825298cfa39a
Sasha <oleksandrsy@gmail.com>
2024-01-14 17:12:23 +0200

    Add project files.

---
+++ FortuneWheel.Domain/WheelSegment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FortuneWheel.Domain
+{
+    public class WheelSegment
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public float Points { get; set; }
+        public string HexColor { get; set; }
+    }
+
+}
---
+++ FortuneWheel.Presentation/Program.cs
@@ -0,0 +1,26 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSession();
+builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseSession();
+app.UseRouting();
+
+app.UseAuthorization();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+});
+app.Run();
