3c880e1d57cd77956cb8aa1daf2e15e0bd9063c4
Sasha <oleksandrsy@gmail.com>
2024-01-17 18:53:25 +0200

    Created UnconfirmedEmail class

--- FortuneWheel.Presentation/Data/DbContexts/IDbContext.cs
+++ FortuneWheel.Presentation/Data/DbContexts/IDbContext.cs
@@ -8,6 +8,7 @@ namespace FortuneWheel.Data.DbContexts
         DbSet<Account> Accounts { get; set; }
         DbSet<WheelOfFortune> WheelsOfFortune { get; set; }
         DbSet<WheelSegment> Segments { get; set; }
+        DbSet<UnconfirmedEmail> UnconfirmedEmails { get; set; }
         Task<int> SaveChangesAsync(CancellationToken token);
         Task<int> SaveChangesAsync();
     }
