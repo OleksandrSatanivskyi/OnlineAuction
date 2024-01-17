08ef36d510e4e2dd96062d20f58e7acfad842659
Sasha <oleksandrsy@gmail.com>
2024-01-17 17:20:42 +0200

    Created IDbContext interface

---
+++ FortuneWheel.Presentation/Data/DbContexts/IDbContext.cs
@@ -0,0 +1,14 @@
+﻿using FortuneWheel.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace FortuneWheel.Data.DbContexts
+{
+    public interface IDbContext
+    {
+        DbSet<Account> Accounts { get; set; }
+        DbSet<WheelOfFortune> WheelsOfFortune { get; set; }
+        DbSet<WheelSegment> Segments { get; set; }
+        Task<int> SaveChangesAsync(CancellationToken token);
+        Task<int> SaveChangesAsync();
+    }
+}
