141bbc34846ee3270323c29ca19be0b5c36ccd8e
Sasha <oleksandrsy@gmail.com>
2024-01-17 13:01:08 +0200

    Created ApplicationDbContext class

--- FortuneWheel.Presentation/Program.cs
+++ FortuneWheel.Presentation/Program.cs
@@ -1,26 +1,72 @@
-var builder = WebApplication.CreateBuilder(args);
+using FortuneWheel.Persistence;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
-builder.Services.AddSession();
-builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+namespace FortuneWheel.Presentation;
 
-var app = builder.Build();
-
-if (!app.Environment.IsDevelopment())
+public class Program
 {
-    app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        ConfigureServices(builder.Services, builder.Configuration);
+
+        var app = builder.Build();
+        ConfigureApp(app);
+
+        app.Run();
+
+    }
+
+    private static void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+        options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+        services.AddSession();
+        services.AddControllersWithViews().AddRazorRuntimeCompilation();
+        services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(options =>
+        {
+            var key = Encoding.UTF8.GetBytes(configuration["Auth:Secret"]);
+            options.SaveToken = true;
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = configuration["Auth:Issuer"],
+                ValidAudience = configuration["Auth:Audience"],
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateLifetime = true,
+                ValidateIssuer = true,
+                ValidateAudience = true,
+            };
+        });
+    }
+
+    private static void ConfigureApp(WebApplication app)
+    {
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Home/Error");
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+        app.UseSession();
+        app.UseRouting();
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+        });
+    }
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-app.UseSession();
-app.UseRouting();
-
-app.UseAuthorization();
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapControllerRoute(
-        name: "default",
-        pattern: "{controller=Home}/{action=Index}/{id?}");
-});
-app.Run();
