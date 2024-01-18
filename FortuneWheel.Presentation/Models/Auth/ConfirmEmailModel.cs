677686307b876365966ec3eb6b9d8f45e26748c5
Sasha <oleksandrsy@gmail.com>
2024-01-18 21:15:47 +0200

    Created ExceptionHandlerMiddleware

--- FortuneWheel.Presentation/Models/Auth/ConfirmEmailModel.cs
+++ FortuneWheel.Presentation/Models/Auth/ConfirmEmailModel.cs
@@ -4,10 +4,8 @@ namespace FortuneWheel.Models.Auth
 {
     public class ConfirmEmailModel
     {
-        [RegularExpression("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$", ErrorMessage = "Invalid email format")]
-        public string Email { get; set; }
-
         [RegularExpression("^[0-9]{5}$", ErrorMessage = "Invalid code format. The code should consist of 5 digits.")]
         public string Code { get; set; }
+        public string? Email { get; set; }
     }
 }
