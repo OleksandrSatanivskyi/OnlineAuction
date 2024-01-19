990e818da5f108e46360d2e98341e6a20a93a186
Sasha <oleksandrsy@gmail.com>
2024-01-19 21:13:01 +0200

    Created IWheelService interface and WheelService class

---
+++ FortuneWheel.Presentation/Services/IWheelService.cs
@@ -0,0 +1,9 @@
+﻿using FortuneWheel.Domain.WheelsOfFortune;
+
+namespace FortuneWheel.Services
+{
+    public interface IWheelService
+    {
+        Task<IEnumerable<WheelOfFortune>> GetUserWheels(Guid userId);
+    }
+}
---
+++ FortuneWheel.Presentation/Services/WheelService.cs
@@ -0,0 +1,20 @@
+﻿using FortuneWheel.Data.DbContexts;
+using FortuneWheel.Domain.WheelsOfFortune;
+
+namespace FortuneWheel.Services
+{
+    public class WheelService : IWheelService
+    {
+        private readonly IDbContext DbContext;
+
+        public WheelService(IDbContext dbContext)
+        {
+            DbContext = dbContext;
+        }
+
+        public Task<IEnumerable<WheelOfFortune>> GetUserWheels(Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
