b4aec528700853a38e57b6d312539e158381d650
Sasha <oleksandrsy@gmail.com>
2024-02-24 18:44:50 +0200

    Created UpdateClassicWheelOptionsModel

---
+++ FortuneWheel.Presentation/Models/Wheels/UpdateClassicWheelOptionsModel.cs
@@ -0,0 +1,10 @@
+﻿using FortuneWheel.Domain.Segments;
+
+namespace FortuneWheel.Models.Wheels
+{
+    public class UpdateClassicWheelOptionsModel
+    {
+        public Guid WheelId { get; set; }
+        public List<Segment> Segments { get; set; }
+    }
+}
