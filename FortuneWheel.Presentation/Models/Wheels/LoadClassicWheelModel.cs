60ba82c65a6122163a072f81c874e735a8cb34c4
Sasha <oleksandrsy@gmail.com>
2024-05-14 14:47:20 +0300

    Fixed changing mode bug

--- FortuneWheel.Presentation/Models/Wheels/LoadClassicWheelModel.cs
+++ FortuneWheel.Presentation/Models/Wheels/LoadClassicWheelModel.cs
@@ -6,7 +6,6 @@ namespace WheelOfFortune.Models.Wheels
 {
     public class LoadClassicWheelModel
     {
-        //public ClassicWheel Wheel { get; set; }
         public Guid WheelId { get; set; }
         public List<Segment> RemainingOptions { get; set; }
 
